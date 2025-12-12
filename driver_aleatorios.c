cc974b021fc083438fba527d2b645fca4d3474ba
Gabriel Derrel <gabriel2077derrel@gmail.com>
2025-12-12 09:57:41 -0300

    alteraçao

--- driver_aleatorios.c
+++ driver_aleatorios.c
@@ -14,12 +14,17 @@ static u32 numero_aleatorio = 0;
 static char buffer_de_escrita_temp[32];
 struct rnd_state estado_aleatorio;
 
+static int abrir_driver(struct inode *inode, struct file *arquivo_aberto){
+    return 0;
+}
+
 static ssize_t ler_driver(struct file *arquivo, char __user *buffer_usuario, size_t tamanho_maximo, loff_t *deslocamento){
     size_t num_bytes_copiados = min(sizeof(u32), tamanho_maximo);
     
     numero_aleatorio = prandom_u32_state(&estado_aleatorio);
 
-    if(copy_to_user(buffer_usuario, &numero_aleatorio, num_bytes_copiados)){ // coloca o número no buffer do usuário
+    // coloca o número no buffer do usuário
+    if(copy_to_user(buffer_usuario, &numero_aleatorio, num_bytes_copiados)){ // sucesso -> retorna 0
         return -EFAULT;
     }
     
@@ -28,18 +33,19 @@ static ssize_t ler_driver(struct file *arquivo, char __user *buffer_usuario, siz
 
 static ssize_t escrever_no_driver(struct file *arquivo, const char __user *buffer_usuario, size_t tamanho_lido, loff_t *deslocamento){
     if(tamanho_lido == 0){
-        return -EINVAL;
+        return -EINVAL; // erro
     }
 
     if(tamanho_lido >= sizeof(buffer_de_escrita_temp)){
-        return -EINVAL;
+        return -EINVAL; // erro
     }
     
-    if(copy_from_user(buffer_de_escrita_temp, buffer_usuario, tamanho_lido)){ // copia a string do buffer do usuário
+    // copia a string do buffer do usuário
+    if(copy_from_user(buffer_de_escrita_temp, buffer_usuario, tamanho_lido)){ // sucesso -> retorna 0
         return -EFAULT;
     }
     
-    buffer_de_escrita_temp[tamanho_lido] = '\0';
+    buffer_de_escrita_temp[tamanho_lido] = '\0'; // torna o vetor de bytes em uma string
     
     if(kstrtou32(buffer_de_escrita_temp, 0, &seed)){ // str to int do kernel
         return -EINVAL;
@@ -49,9 +55,10 @@ static ssize_t escrever_no_driver(struct file *arquivo, const char __user *buffe
     return tamanho_lido;
 }
 
-// Estrutura de operações do dispositivo
+// operações do driver
 static struct file_operations operacoes = {
     .owner = THIS_MODULE,
+    .open = abrir_driver,
     .read = ler_driver,
     .write = escrever_no_driver,
 };
@@ -65,17 +72,19 @@ static int __init inicializar(void){
     seed = (u32) jiffies;
     prandom_seed_state(&estado_aleatorio, seed); // seta o estado aleatório do prandom com a seed
     
+    // cria o driver
     device_create(NULL, NULL, MKDEV(id_driver, 0), NULL, NOME);
     
     return 0;
 }
 
 static void __exit sair(void){
+    // destroi o driver
     device_destroy(NULL, MKDEV(id_driver, 0));
-    unregister_chrdev(id_driver, NOME);
+    unregister_chrdev(id_driver, NOME); // retira o degistro do /dev
 }
 
-module_init(inicializar);
-module_exit(sair);
+module_init(inicializar); // seta função de inicialização
+module_exit(sair); // seta função de fechamento
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL"); // define a licença, para poder utilizar os metodos do kernel
